711ab32a0c40e00b4be8cef043be1f2f041b2964
verayu <verayu@insynerger.com>
2022-07-10 22:44:16 +0800

    商品功能 - 實作查詢商品列表(排序)

--- src/main/java/com/vera/springbootmall/controller/ProductController.java
+++ src/main/java/com/vera/springbootmall/controller/ProductController.java
@@ -21,12 +21,18 @@ public class ProductController {
 
     @GetMapping("products")
     public ResponseEntity<List<Product>>getProduces(
+            //查詢條件 Filtering
             @RequestParam(required = false) ProductCategory category,
-            @RequestParam(required = false) String search
+            @RequestParam(required = false) String search,
+            //排序 Sorting
+            @RequestParam(defaultValue = "created_date") String orderBy,
+            @RequestParam(defaultValue = "desc") String sort
     ){
         ProductQueryParams productQueryParams = new ProductQueryParams();
         productQueryParams.setCategory(category);
         productQueryParams.setSearch(search);
+        productQueryParams.setOrderBy(orderBy);
+        productQueryParams.setSort(sort);
 
         List<Product> productList = productService.getProducts(productQueryParams);
         //實作列表型api時 不論有沒有查到數據 都要固定回傳200ok給前端
--- src/main/java/com/vera/springbootmall/dto/ProductQueryParams.java
+++ src/main/java/com/vera/springbootmall/dto/ProductQueryParams.java
@@ -5,6 +5,8 @@ import com.vera.springbootmall.constant.ProductCategory;
 public class ProductQueryParams {
     private ProductCategory category;
     private String search;
+    private String OrderBy;
+    private String sort;
 
     public ProductCategory getCategory() {
         return category;
@@ -12,6 +14,22 @@ public class ProductQueryParams {
 
     public void setCategory(ProductCategory category) {
         this.category = category;
+    }
+
+    public String getOrderBy() {
+        return OrderBy;
+    }
+
+    public void setOrderBy(String orderBy) {
+        OrderBy = orderBy;
+    }
+
+    public String getSort() {
+        return sort;
+    }
+
+    public void setSort(String sort) {
+        this.sort = sort;
     }
 
     public String getSearch() {
