b97437830d6f4968344bc5d56bffe79ba9a3940e
verayu <verayu@insynerger.com>
2022-09-06 22:27:52 +0800

    商品功能 - 實作查詢商品列表(分頁)

--- src/main/java/com/vera/springbootmall/controller/ProductController.java
+++ src/main/java/com/vera/springbootmall/controller/ProductController.java
@@ -8,11 +8,15 @@ import com.vera.springbootmall.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import java.util.List;
 
+@Validated
 @RestController
 public class ProductController {
 
@@ -26,13 +30,21 @@ public class ProductController {
             @RequestParam(required = false) String search,
             //排序 Sorting
             @RequestParam(defaultValue = "created_date") String orderBy,
-            @RequestParam(defaultValue = "desc") String sort
+            @RequestParam(defaultValue = "desc") String sort,
+
+            //分頁Pagination
+            //limit表示要取得幾筆商品數據
+            //offset表示要跳過多少比數據
+            @RequestParam(defaultValue = "5") @Max(1000) @Min(0) Integer limit,
+            @RequestParam(defaultValue = "0") @Min(0) Integer offset
     ){
         ProductQueryParams productQueryParams = new ProductQueryParams();
         productQueryParams.setCategory(category);
         productQueryParams.setSearch(search);
         productQueryParams.setOrderBy(orderBy);
         productQueryParams.setSort(sort);
+        productQueryParams.setLimit(limit);
+        productQueryParams.setOffset(offset);
 
         List<Product> productList = productService.getProducts(productQueryParams);
         //實作列表型api時 不論有沒有查到數據 都要固定回傳200ok給前端
--- src/main/java/com/vera/springbootmall/dao/impl/ProductDaoImpl.java
+++ src/main/java/com/vera/springbootmall/dao/impl/ProductDaoImpl.java
@@ -41,7 +41,13 @@ public class ProductDaoImpl implements ProductDao {
             map.put("search","%" + productQueryParams.getSearch() + "%");
         }
 
+        //排序
         sql = sql + " ORDER BY " + productQueryParams.getOrderBy() + " " + productQueryParams.getSort();
+
+        //分頁
+        sql = sql + " LIMIT :limit OFFSET :offset";
+        map.put("limit",productQueryParams.getLimit());
+        map.put("offset",productQueryParams.getOffset());
 
         List<Product> productList = namedParameterJdbcTemplate.query(sql,map,new ProductRowMapper());
         return productList;
