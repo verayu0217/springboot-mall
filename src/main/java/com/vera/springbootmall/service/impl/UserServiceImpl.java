1166770f0416a4814754d38ae7e34326e0b363b8
INSYNERGER\verayu <verayu@insynerger.com>
2023-08-22 14:10:45 +0800

    帳號功能 - 使用 MD5 加密密碼

--- src/main/java/com/vera/springbootmall/service/impl/UserServiceImpl.java
+++ src/main/java/com/vera/springbootmall/service/impl/UserServiceImpl.java
@@ -10,6 +10,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.util.DigestUtils;
 import org.springframework.web.server.ResponseStatusException;
 
 @Component
@@ -36,6 +37,11 @@ public class UserServiceImpl  implements UserService {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
         }
 
+        //使用 MD5 生成密碼雜湊值
+        //getBytes將字串轉成byte類型
+        String hashedPassword = DigestUtils.md5DigestAsHex(userRegisterRequest.getPassword().getBytes());
+        userRegisterRequest.setPassword(hashedPassword);
+
         //創建帳號
         return userDao.createUser(userRegisterRequest);
     }
@@ -51,8 +57,11 @@ public class UserServiceImpl  implements UserService {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
         }
 
+        //使用 MD5 生成密碼雜湊值
+        String hashedPassword = DigestUtils.md5DigestAsHex(userLoginRequest.getPassword().getBytes());
+
         //檢查資料庫中user的密碼與前端傳過來的密碼是否一樣
-        if (user.getPassword().equals(userLoginRequest.getPassword())){
+        if (user.getPassword().equals(hashedPassword)){
             return user;
         }else{
             log.warn("email {} 的密碼不正確",userLoginRequest.getEmail());
